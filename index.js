6e16d689ea6ca6ceea70070066a788c907d72c19
root <root@monitor-app.localdomain>
2017-07-19 10:14:14 +0000

    Go!

---
+++ index.js
@@ -0,0 +1,38 @@
+const lighthouse = require('lighthouse');
+const chromeLauncher = require('lighthouse/chrome-launcher');
+const http = require('http');
+const url = require('url');
+
+const flags = {
+  chromeFlags: ['--headless', '--disable-gpu', '--no-sandbox']
+};
+
+const responseHeaders = {
+  'Content-Type': 'application/json'
+};
+
+function requestHandler(request, response) {
+  var query = url.parse(request.url, true).query;
+
+  if ( ! query.url ) {
+    response.writeHead(400, responseHeaders);
+    response.end(JSON.stringify({'error': 'Missing the URL query parameter.'}));
+    return;
+  }
+
+  launchChromeAndRunLighthouse('https://kaspars.net', flags).then(results => {
+    response.writeHead(200, responseHeaders);
+    response.end(JSON.stringify(results));
+  });
+}
+
+function launchChromeAndRunLighthouse(url, flags = {}, config = null) {
+  return chromeLauncher.launch(flags).then(chrome => {
+    flags.port = chrome.port;
+
+    return lighthouse(url, flags, config).then(results =>
+      chrome.kill().then(() => results));
+  });
+}
+
+http.createServer(requestHandler).listen( 80 );
---
+++ readme.md
@@ -0,0 +1,6 @@
+# Lighthouse API Server
+
+## Usage
+
+	$ git clone https://github.com/kasparsd/bluelight.git
+	$ nohup node bluelight/index.js > bluelight.log & 
